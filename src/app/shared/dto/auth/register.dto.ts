615a80b94befc11428f52ea2b1738ce3d0034901
jhon azcarate <jhonazcaratework@gmail.com>
2025-03-13 16:24:53 -0500

    añadimos los servicios de registros y toogle user.

--- src/app/shared/dto/auth/register.dto.ts
+++ src/app/shared/dto/auth/register.dto.ts
@@ -1,12 +1,12 @@
 import { TipoDocumentoEnum } from "@shared/enum/tipo-documento.enum";
 
 export interface RegisterDto{
-    documento:number;
+    per_documento:number;
     email:string;
-    tipo_documento:TipoDocumentoEnum;
-    nombre:string;
-    segundo_nombre?:string;
-    apellido:string;
-    segundo_apellido?:string;
-    foto_perfil?:File;
+    per_tipo_documento:TipoDocumentoEnum;
+    password:string;
+    password2?:string;
+    per_nombres:string;
+    per_apellidos:string;
+    per_telefono:string;
 }
--- src/app/shared/services/admin-usuarios.service.ts
+++ src/app/shared/services/admin-usuarios.service.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
+import { RegisterDto } from '@shared/dto/auth/register.dto';
 import { getQueryUrl } from '@shared/functions/url.functions';
 import { PaginateModel } from '@shared/models/paginate.model';
 import { PersonaModel } from '@shared/models/persona.model';
@@ -17,6 +18,16 @@ export class AdminUsuariosService {
    getAll(data?: QueryUrlModel) {
       let url: string = getQueryUrl(this.url, data);
       return this.http.get<PaginateModel<PersonaModel>>(url);
+  }
+
+  toggleUser(document:number) {
+    let url: string = `${this.url}desactivar/${document}`;
+    return this.http.post<PersonaModel>(url,{});
+  }
+
+  create(usuario:RegisterDto){
+    let url: string = `${this.url}registrar`;
+    return this.http.post<PersonaModel>(url,usuario);
   }
 
 }
