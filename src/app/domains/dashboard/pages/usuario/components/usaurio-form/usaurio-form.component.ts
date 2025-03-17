44cfd93198ac323bc36c747f38b4afda91bf3645
Jhon Wilmer Azcarate Rivera <jhonazcaratework@gmail.com>
2025-03-16 19:48:57 -0500

    register form.

---
+++ src/app/domains/dashboard/pages/usuario/components/usaurio-form/usaurio-form.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-usaurio-form',
+  standalone: true,
+  imports: [],
+  templateUrl: './usaurio-form.component.html',
+  styleUrl: './usaurio-form.component.css'
+})
+export class UsaurioFormComponent {
+
+}
